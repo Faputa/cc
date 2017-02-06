fe34283b59a77e16f712fa343f8b3cfef1875b46
kinocastle <2238932729@qq.com>
2017-02-06 23:54:09 +0800

    Add files via upload

--- test/test3.c
+++ test/test3.c
@@ -3,14 +3,14 @@
 int f(int a, int b, int c) {
 	int d, e, f;
 }
-int a = 100, b = 200, c = 300;
+int a = 100, b = -200, c = 300;
 int main() {
 	print(a); space(); print(b);
 	endl();
 	int a;
 	{
 		int b;
-		a = 100 * b = 100 * 100;
+		a = -100 * b = -100 * 100;
 		print(a); space(); print(b);
 	}
 }
